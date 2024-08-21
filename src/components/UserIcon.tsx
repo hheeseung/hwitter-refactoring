7e92666e42e73895af743dbc560c7db4b6367d24
Heeseung Ha <ssigu69@gmail.com>
2024-08-21 16:54:19 +0900

    feat: 프로필 이미지 없을 경우 대체 아이콘 추가

---
+++ src/components/UserIcon.tsx
@@ -0,0 +1,7 @@
+import { FaUser } from 'react-icons/fa';
+
+export default function UserIcon() {
+  return (
+    <FaUser className='size-11 text-primary bg-background p-2 rounded-xl shadow-sm' />
+  );
+}
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/lib/db';
 import { getSession } from '@/lib/session';
 import Image from 'next/image';
+import UserIcon from './UserIcon';
 
 export default async function UserInfo() {
   const { id } = await getSession();
@@ -21,13 +22,17 @@ export default async function UserInfo() {
     <article className='bg-white flex items-center gap-3 w-full rounded-xl p-4 shadow-md mb-3'>
       {user ? (
         <>
-          <Image
-            src={user.profileImg ?? ''}
-            alt='profile'
-            width={45}
-            height={45}
-            className='rounded-xl'
-          />
+          {user.profileImg ? (
+            <Image
+              src={user.profileImg}
+              alt='profile'
+              width={45}
+              height={45}
+              className='rounded-xl'
+            />
+          ) : (
+            <UserIcon />
+          )}
           <div className='flex flex-col'>
             <span className='font-semibold'>{user.username}</span>
             <span className='text-slate-500 text-sm'>{user.email}</span>
