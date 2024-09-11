5fd8933b9aee199d52b59b101045108cf3163c06
Heeseung Ha <ssigu69@gmail.com>
2024-09-11 14:22:48 +0900

    refactor: 상단 로고 클릭 영역 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,13 +4,12 @@ import LogoIcon from '../ui/LogoIcon';
 export default function Header() {
   return (
     <header className='bg-white w-full p-4'>
-      <Link
-        href='/'
-        className='flex gap-3 items-center max-w-screen-2xl mx-auto'
-      >
-        <LogoIcon />
-        <span className='font-bold text-3xl'>Hwitter</span>
-      </Link>
+      <div className='max-w-screen-2xl mx-auto'>
+        <Link href='/' className='flex items-center gap-3 w-fit'>
+          <LogoIcon />
+          <span className='font-bold text-3xl'>Hwitter</span>
+        </Link>
+      </div>
     </header>
   );
 }
